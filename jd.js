9a9bd2e1f231aeb667d8d5447eaa096e7e23f0b1
jdcoffre <jdcoffre@gmail.com>
2025-01-02 15:59:20 +0100

    A few update + dark theme

---
+++ jd.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const htmlElement = document.documentElement;
+    const switchElement = document.getElementById('darkThemeSwitch');
+
+    const currentTheme = localStorage.getItem('bsTheme') || 'light';
+    htmlElement.setAttribute('data-bs-theme', currentTheme);
+    switchElement.checked = currentTheme === 'dark';
+
+    switchElement.addEventListener('change', function () {
+        if (this.checked) {
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('bsTheme', 'dark');
+        } else {
+            htmlElement.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('bsTheme', 'light');
+        }
+    });
+});
